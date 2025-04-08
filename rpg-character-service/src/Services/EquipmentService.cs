9cd31db6ab577f5c25cc4d0e2c2015f6f1fd8ae7
Daniel Casadevall Pino <dcasadevall@me.com>
2025-04-08 11:44:11 -0700

    Fix equipment service

--- rpg-character-service/src/Services/EquipmentService.cs
+++ rpg-character-service/src/Services/EquipmentService.cs
@@ -1,3 +1,4 @@
+using RPGCharacterService.Exceptions.Character;
 using RPGCharacterService.Exceptions.Items;
 using RPGCharacterService.Models.Characters;
 using RPGCharacterService.Models.Items;
@@ -16,8 +17,9 @@ namespace RPGCharacterService.Services {
     /// <param name="itemId">The unique identifier of the weapon to equip.</param>
     /// <param name="offHand">Whether to equip the weapon in the off-hand slot.</param>
     /// <returns>The updated character with the new equipment configuration.</returns>
-    /// <exception cref="EquipmentTypeMismatchException">Thrown when the item is not a weapon.</exception>
     /// <exception cref="CharacterNotFoundException">Thrown when no character exists with the specified ID.</exception>
+    /// <exception cref="ItemNotFoundException">Thrown when no item exists with the specified ID.</exception>
+    /// <exception cref="EquipmentTypeMismatchException">Thrown when the item is not a weapon.</exception>
     Task<Character> EquipWeaponAsync(Guid characterId, int itemId, bool offHand = false);
 
     /// <summary>
@@ -26,8 +28,9 @@ namespace RPGCharacterService.Services {
     /// <param name="characterId">The unique identifier of the character.</param>
     /// <param name="itemId">The unique identifier of the armor to equip.</param>
     /// <returns>The updated character with the new equipment configuration.</returns>
-    /// <exception cref="EquipmentTypeMismatchException">Thrown when the item is not armor.</exception>
     /// <exception cref="CharacterNotFoundException">Thrown when no character exists with the specified ID.</exception>
+    /// <exception cref="ItemNotFoundException">Thrown when no item exists with the specified ID.</exception>
+    /// <exception cref="EquipmentTypeMismatchException">Thrown when the item is not a weapon.</exception>
     Task<Character> EquipArmorAsync(Guid characterId, int itemId);
 
     /// <summary>
@@ -36,8 +39,9 @@ namespace RPGCharacterService.Services {
     /// <param name="characterId">The unique identifier of the character.</param>
     /// <param name="itemId">The unique identifier of the shield to equip.</param>
     /// <returns>The updated character with the new equipment configuration.</returns>
-    /// <exception cref="EquipmentTypeMismatchException">Thrown when the item is not a shield.</exception>
     /// <exception cref="CharacterNotFoundException">Thrown when no character exists with the specified ID.</exception>
+    /// <exception cref="ItemNotFoundException">Thrown when no item exists with the specified ID.</exception>
+    /// <exception cref="EquipmentTypeMismatchException">Thrown when the item is not a weapon.</exception>
     Task<Character> EquipShieldAsync(Guid characterId, int itemId);
   }
 
@@ -52,17 +56,23 @@ namespace RPGCharacterService.Services {
     /// </summary>
     /// <param name="characterId">The unique identifier of the character.</param>
     /// <param name="itemId">The unique identifier of the weapon to equip.</param>
-    /// <param name="offhand">Whether to equip the weapon in the off-hand slot.</param>
+    /// <param name="offHand">Whether to equip the weapon in the off-hand slot.</param>
     /// <returns>The updated character with the new equipment configuration.</returns>
-    /// <exception cref="EquipmentTypeMismatchException">Thrown when the item is not a weapon.</exception>
     /// <exception cref="CharacterNotFoundException">Thrown when no character exists with the specified ID.</exception>
+    /// <exception cref="ItemNotFoundException">Thrown when no item exists with the specified ID.</exception>
+    /// <exception cref="EquipmentTypeMismatchException">Thrown when the item is not a weapon.</exception>
     public async Task<Character> EquipWeaponAsync(Guid characterId, int itemId, bool offHand = false) {
       // Load the item and character
       var itemTask = itemRepository.GetByIdOrThrowAsync(itemId);
       var characterTask = characterRepository.GetByIdOrThrowAsync(characterId);
-      var (item, character) = await Task.WhenAll(itemTask, characterTask);
+      await Task.WhenAll(itemTask, characterTask);
 
+      // Equip the weapon
+      var item = itemTask.Result;
+      var character = characterTask.Result;
       character.Equipment.EquipWeapon(item, offHand);
+
+      // Update the character in the repository
       await characterRepository.UpdateAsync(character);
       return character;
     }
@@ -73,15 +83,21 @@ namespace RPGCharacterService.Services {
     /// <param name="characterId">The unique identifier of the character.</param>
     /// <param name="itemId">The unique identifier of the armor to equip.</param>
     /// <returns>The updated character with the new equipment configuration.</returns>
-    /// <exception cref="EquipmentTypeMismatchException">Thrown when the item is not armor.</exception>
     /// <exception cref="CharacterNotFoundException">Thrown when no character exists with the specified ID.</exception>
+    /// <exception cref="ItemNotFoundException">Thrown when no item exists with the specified ID.</exception>
+    /// <exception cref="EquipmentTypeMismatchException">Thrown when the item is not a weapon.</exception>
     public async Task<Character> EquipArmorAsync(Guid characterId, int itemId) {
       // Load the item and character
       var itemTask = itemRepository.GetByIdOrThrowAsync(itemId);
       var characterTask = characterRepository.GetByIdOrThrowAsync(characterId);
-      var (item, character) = await Task.WhenAll(itemTask, characterTask);
+      await Task.WhenAll(itemTask, characterTask);
 
+      // Equip the armor
+      var item = itemTask.Result;
+      var character = characterTask.Result;
       character.Equipment.EquipArmor(item);
+
+      // Update the character in the repository
       await characterRepository.UpdateAsync(character);
       return character;
     }
@@ -92,15 +108,21 @@ namespace RPGCharacterService.Services {
     /// <param name="characterId">The unique identifier of the character.</param>
     /// <param name="itemId">The unique identifier of the shield to equip.</param>
     /// <returns>The updated character with the new equipment configuration.</returns>
-    /// <exception cref="EquipmentTypeMismatchException">Thrown when the item is not a shield.</exception>
     /// <exception cref="CharacterNotFoundException">Thrown when no character exists with the specified ID.</exception>
+    /// <exception cref="ItemNotFoundException">Thrown when no item exists with the specified ID.</exception>
+    /// <exception cref="EquipmentTypeMismatchException">Thrown when the item is not a weapon.</exception>
     public async Task<Character> EquipShieldAsync(Guid characterId, int itemId) {
       // Load the item and character
       var itemTask = itemRepository.GetByIdOrThrowAsync(itemId);
       var characterTask = characterRepository.GetByIdOrThrowAsync(characterId);
-      var (item, character) = await Task.WhenAll(itemTask, characterTask);
+      await Task.WhenAll(itemTask, characterTask);
 
+      // Equip the shield
+      var item = itemTask.Result;
+      var character = characterTask.Result;
       character.Equipment.EquipShield(item);
+
+      // Update the character in the repository
       await characterRepository.UpdateAsync(character);
       return character;
     }
