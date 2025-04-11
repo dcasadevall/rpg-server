8a4ceb2f0f2f6b00594437e5d5fd61de2969a40f
Daniel Casadevall Pino <dcasadevall@me.com>
2025-04-11 02:24:49 -0700

    clarify health check

--- rpg-character-service/src/Controllers/HealthCheckController.cs
+++ rpg-character-service/src/Controllers/HealthCheckController.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace RPGCharacterService.Controllers {
   /// <summary>
   /// I am sure there is a better way to implement a health check, but this was the simplest :).
+  /// This HealthCheck class has nothing to do with our application, but instead is used to serve
+  /// your typical health check endpoint.
   /// </summary>
   [ApiController]
   [Route("health")]
-  public class HealthController : ControllerBase {
+  public class HealthCheckController : ControllerBase {
     [HttpGet("ready")]
     public IActionResult Ready() {
       return Ok(new {status = "ready"});
